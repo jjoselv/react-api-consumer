d2e963deb519284fb581ca52a32efceca73f8892
jjoselv <jjoselvill@gmail.com>
2021-01-30 11:52:07 +0100

    feat: Add 404 page and move to npm (ts-errors)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,22 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import NotFound from '../NotFound';
 import './App.scss';
 
 function App() {
   return (
     <div className="container">
-    <Router>
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-      <Switch>
-       
-        {/* React Router api dictates using children prop for permanent mount (no remounting no matter the route) */}
-        {/* eslint-disable-next-line react/no-children-prop */}
-        <Route path="/" children={() => <h1>Title</h1>} />
-      </Switch>
-    </Router>
-  </div>
+      <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        
+          {/* React Router api dictates using children prop for permanent mount (no remounting no matter the route) */}
+          {/* eslint-disable-next-line react/no-children-prop */}
+          <Route exact path="/" children={() => <h1>Title</h1>} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
